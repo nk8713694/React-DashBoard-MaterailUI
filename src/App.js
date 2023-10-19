23af8ca91f47f29f7879b783c72b21e3bdb90d19
nk8713694 <nk8713694@gmail.com>
2023-10-19 11:27:49 +0530

    feat: :recycle: added code for different routes like home settings and about

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
 
 import SideNav from './components/SideNav';
+
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Settings from './pages/Settings';
 
 function App() {
   return (
-    <div className='App'>
-      <SideNav></SideNav>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' exact element={<Home />}></Route>
+        <Route path='/about' exact element={<About />}></Route>
+
+        <Route path='/settings' exact element={<Settings />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import SideNav from '../components/SideNav';
+import Typography from '@mui/material/Typography';
+import { styled, useTheme } from '@mui/material/styles';
 
 const Home = () => {
-  return <div>Home</div>;
+  const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  }));
+  return (
+    <div>
+      <Box sx={{ display: 'flex' }}>
+        <SideNav></SideNav>
+        {/* <h3>home</h3> */}
+      </Box>
+    </div>
+  );
 };
 
 export default Home;
